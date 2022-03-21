ed03e9801750fe232d8738c78a30f58fb3d4e469
whywhywhyw <97542086+whywhywhyw@users.noreply.github.com>
2022-03-21 10:40:53 -0500

    Update ExampleMixin.java

--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.whywhywhyw.test.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.whywhywhyw.test.sus;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;

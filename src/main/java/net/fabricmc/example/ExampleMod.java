2571fac80cbb3fb7b4f7554c86c91141f3ae8c20
whywhywhyw <97542086+whywhywhyw@users.noreply.github.com>
2022-03-21 10:32:30 -0500

    Update ExampleMod.java

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example;
+package net.whywhywhyw.test;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -16,6 +16,6 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Discord Ping");
 	}
 }
